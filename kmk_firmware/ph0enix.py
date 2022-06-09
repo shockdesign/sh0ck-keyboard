3830de7faf2ec37fa492f0934d9e7ff488ad1406
Rajesh Singh <rajesh@shockdesign.com.au>
2022-06-09 14:06:41 +1000

    Updated firmware to now include missing keys and proper escape

--- kmk_firmware/ph0enix.py
+++ kmk_firmware/ph0enix.py
@@ -35,7 +35,7 @@ keyboard.keymap = [
     # | Ctrl |  Alt |  GUI | Space|   FN | Space|   /  | Left | Down | Right| Bksp |
     # `----------------------------------------------------------------------------'
     [
-        KC.GESC,  KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,   KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,
+        KC.ESC,   KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,   KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,
         KC.TAB,   KC.A,    KC.S,    KC.D,    KC.F,    KC.G,   KC.H,    KC.J,    KC.K,    KC.L,    KC.ENT,
         KC.LSFT,  KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,   KC.N,    KC.M,    KC.COMM, KC.UP,   KC.DOT,
         KC.LCTRL, KC.LALT, KC.LGUI, KC.SPC,  FN,      KC.SPC, KC.SLSH, KC.LEFT, KC.DOWN, KC.RGHT, KC.BSPC,
@@ -44,17 +44,17 @@ keyboard.keymap = [
     # ,----------------------------------------------------------------------------.
     # | Grave|   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |
     # |------+------+------+------+------+------+------+------+------+------+------|
-    # | Trans|   F1 |   F3 |   F5 |   F7 |   F9 |  F11 | Trans| Trans| Trans| Trans|
+    # | Trans|   F1 |   F3 |   F5 |   F7 |   F9 |  F11 |   -  |   ;  |   '  | Trans|
     # |------+------+------+------+------+-------------+------+------+------+------|
-    # | Trans|   F2 |   F4 |   F6 |   F8 |  F10 |  F12 | Trans|   (  | Trans|   )  |
+    # | Trans|   F2 |   F4 |   F6 |   F8 |  F10 |  F12 |   =  |   (  | Pg Up|   )  |
     # |------+------+------+------+------+------+------+------+------+------+------|
-    # | Trans| Trans| Trans| Trans| Trans| Trans|   \  | Trans| Trans| Trans| Trans|
+    # | Trans| Trans| Trans| Trans| Trans| Trans|   \  | Home | Pg Dn|  End |  Del |
     # `----------------------------------------------------------------------------'
     [
         KC.GRV,  KC.N1,   KC.N2,   KC.N3,    KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,
-        KC.TRNS, KC.F1,   KC.F3,   KC.F5,    KC.F7,   KC.F9,   KC.F11,  KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
-        KC.TRNS, KC.F2,   KC.F4,   KC.F6,    KC.F8,   KC.F10,  KC.F12,  KC.TRNS, KC.LBRC, KC.TRNS, KC.RBRC,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,  KC.TRNS, KC.TRNS, KC.BSLS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.F1,   KC.F3,   KC.F5,    KC.F7,   KC.F9,   KC.F11,  KC.MINS, KC.SCLN, KC.QUOT, KC.TRNS,
+        KC.TRNS, KC.F2,   KC.F4,   KC.F6,    KC.F8,   KC.F10,  KC.F12,  KC.EQL,  KC.LBRC, KC.PGUP, KC.RBRC,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,  KC.TRNS, KC.TRNS, KC.BSLS, KC.HOME, KC.PGDN, KC.END,  KC.DEL,
     ],
 ]
 
